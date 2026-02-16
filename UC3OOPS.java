7a9830bee20a0fe4ea0cab5a90b1ba9ea363d8af
st0955-source <st0955@srmist.edu.in>
2026-02-16 09:26:50 +0530

    Add files via upload

---
+++ UC3OOPS.java
@@ -0,0 +1,13 @@
+public class UC3OOPS{
+	public static void main(String[] args){
+		System.out.println(String.join(" ","    ***   " , "    ***   " , "***** " , "  ***** "));
+		System.out.println(String.join(" "," **    ** " , " **    ** " , "*   **" , " **     "));
+		System.out.println(String.join(" ","**      **" , "**      **" , "*   **" , "**      "));
+		System.out.println(String.join(" ","**      **" , "**      **" , "*  ** " , " *****  "));
+		System.out.println(String.join(" ","**      **" , "**      **" , "***   " , "     ** "));
+		System.out.println(String.join(" "," **    ** " , " **    ** " , "*     " , " **  ** "));
+		System.out.println(String.join(" ","   ***    " , "   ***    " , "*     " , "  ***** "));
+	
+		}
+	
+	}
